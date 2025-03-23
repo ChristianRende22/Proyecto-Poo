f388bad86033b4a990e80463514bcb4eaf86495b
Christian <111664687+ChristianRende22@users.noreply.github.com>
2025-03-22 22:54:08 -0600

    Add files via upload
    Javadoc

---
+++ JavadocClinica/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"modelo","l":"Cita"},{"p":"main","l":"ClinicaDental"},{"p":"modelo","l":"Doctor"},{"p":"modelo","l":"Factura"},{"p":"modelo","l":"Horario"},{"p":"modelo","l":"Paciente"},{"p":"modelo","l":"Tratamiento"}];updateSearchResults();
